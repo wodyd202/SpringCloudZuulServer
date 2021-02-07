703075c09845399d0bc7402b1555250d411b94a0
wodyd202 <wodyd202@naver.com>
2021-02-07 18:25:00 +0900

    - 프로젝트 시작 - zuul 기본설정 완료

---
+++ SpringCloudGateway/src/main/java/com/ljy/gateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.ljy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
+
+}
