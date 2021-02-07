adcf0ab4ec4ea78e9d6db2459180368b61b2ffa6
wodyd202 <wodyd202@naver.com>
2021-02-07 22:26:17 +0900

    - refreshScope 추가

--- SpringCloudGateway/src/main/java/com/ljy/gateway/SpringCloudGatewayApplication.java
+++ SpringCloudGateway/src/main/java/com/ljy/gateway/SpringCloudGatewayApplication.java
@@ -2,10 +2,12 @@ package com.ljy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@RefreshScope
 public class SpringCloudGatewayApplication {
 
 	public static void main(String[] args) {
